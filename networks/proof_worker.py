8e9ecf23923c229c929189db6535b60a55d2d3b1
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-04-22 14:51:50 +0300

    test

--- networks/proof_worker.py
+++ networks/proof_worker.py
@@ -1,20 +1,10 @@
-import base64
-from hashlib import sha3_512
-import json
 import random
-
 from datetime import datetime, timedelta, timezone
-
 from constants.headers import OPENAI_GET_HEADERS
 
-
 class ProofWorker:
-    def __init__(self, user_name, difficulty=None, required=False, seed=None):
-        self.user_name = user_name
-        self.difficulty = difficulty
-        self.required = required
-        self.seed = seed
-        self.proof_token_prefix = "gAAAAABwQ8Lk5FbGpA2NcR9dShT6gYjU7VxZ4D"
+    def __init__(self):
+        pass
 
     def get_parse_time(self):
         now = datetime.now()
@@ -36,33 +26,7 @@ class ProofWorker:
             OPENAI_GET_HEADERS["User-Agent"],
         ]
 
-    def calc_proof_token(self, seed: str, difficulty: str):
-        try:
-            config = self.get_config()
-            diff_len = len(difficulty) // 2
-            for i in range(100000):
-                config[3] = i
-                json_str = json.dumps(config)
-                base = base64.b64encode(json_str.encode()).decode()
-                hasher = sha3_512()
-                hasher.update((seed + base).encode())
-                hash_val = hasher.digest().hex()
-                if hash_val[:diff_len] <= difficulty:
-                    return "gAAAAAB" + base
-            self.proof_token = (
-                self.proof_token_prefix + base64.b64encode(seed.encode()).decode()
-            )
-            return self.proof_token
-        except Exception as e:
-            return str(e)
-
-
 if __name__ == "__main__":
-    user_name = "Niansuh"
-    seed, difficulty = "0.42665582693491433", "05cdf2"
-    worker = ProofWorker(user_name)
-    proof_token = worker.calc_proof_token(seed, difficulty)
-    decoded_proof_token = base64.b64decode(proof_token.encode()).decode()
-    print(f"proof_token: {proof_token}")
-    print(f"decoded_proof_token: {decoded_proof_token}")
-    # python -m networks.proof_worker
+    worker = ProofWorker()
+    config = worker.get_config()
+    print("Config:", config)
